29e57271a8dee8349618097e22a47e0d2ff3f632
Kir Shatrov <shatrov@me.com>
2015-05-18 08:27:20 +0300

    Initial commit

---
+++ hello.sbt
@@ -0,0 +1,5 @@
+lazy val root = (project in file(".")).
+settings(
+  name := "hello",
+  version := "1.0"
+)
---
+++ hw.scala
@@ -0,0 +1,7 @@
+import finance.currencies._
+
+object Hi {
+  def main(args: Array[String]) =
+    println("Hi!")
+    println(Currency.values)
+}
